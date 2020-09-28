55620b0d0918810790d8b0d23868eebbed9e1637
darkkaiser <darkkaiser@gmail.com>
2020-09-28 13:40:43 +0900

    .

--- service/task/task_lotto.go
+++ service/task/task_lotto.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/darkkaiser/notify-server/g"
+	"github.com/darkkaiser/notify-server/utils"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
@@ -102,16 +103,24 @@ func (t *lottoTask) runPrediction(taskData interface{}) (message string, changed
 
 	// 일정 시간마다 사용자가 작업을 취소하였는지의 여부를 확인한다.
 	ticker := time.NewTicker(time.Millisecond * 500)
-	defer ticker.Stop()
+	tickerStopC := make(chan bool, 1)
 
 	go func(ticker *time.Ticker, cmd *exec.Cmd) {
-		for range ticker.C {
-			if t.IsCanceled() == true {
-				err0 := cmd.Process.Signal(os.Kill)
-				if err0 != nil {
-					log.Errorf("사용자 요청으로 작업을 취소하는 중에 실행중인 외부 프로그램의 종료가 실패하였습니다.(error:%s)", err0)
+		for {
+			select {
+			case <-ticker.C:
+				if t.IsCanceled() == true {
+					ticker.Stop()
+					err0 := cmd.Process.Signal(os.Kill)
+					if err0 != nil {
+						log.Errorf("사용자 요청으로 작업을 취소하는 중에 실행중인 외부 프로그램의 종료가 실패하였습니다.(error:%s)", err0)
+					}
+					return
 				}
-				break
+
+			case <-tickerStopC:
+				ticker.Stop()
+				return
 			}
 		}
 	}(ticker, cmd)
@@ -119,50 +128,52 @@ func (t *lottoTask) runPrediction(taskData interface{}) (message string, changed
 	// 작업이 완료될 때까지 대기한다.
 	err = cmd.Wait()
 	if err != nil {
+		tickerStopC <- true
+
 		// 작업 진행중에 사용자가 작업을 취소한 경우...
 		if t.IsCanceled() == true {
 			return "", nil, nil
 		}
 
 		return "", nil, err
+	} else {
+		tickerStopC <- true
 	}
 
-	// @@@@@
-	///////////////////////////////////
-	// 작업 결과를 받아온다.
-	// 로또 당첨번호 예측작업이 종료되었습니다. 5개의 대상 당첨번호가 추출되었습니다.(최대 당첨가능 확률값:0.1, 경로:F:\Solutions-Private\lotto-prediction\dist\prediction-logs\931_20200927_233542591_predict.log)
 	cmdOutString := cmdOutBuffer.String()
+
+	// 당첨번호 예측 결과가 저장되어 있는 파일의 경로를 추출한다.
 	analysisFilePath := regexp.MustCompile("로또 당첨번호 예측작업이 종료되었습니다. [0-9]+개의 대상 당첨번호가 추출되었습니다.\\((.*)\\)").FindString(cmdOutString)
 	if len(analysisFilePath) == 0 {
-		return "", nil, errors.New("")
+		return "", nil, errors.New(fmt.Sprint("당첨번호 예측 작업이 정상적으로 완료되었는지 확인할 수 없습니다. 자세한 내용은 로그를 확인하여 주세요."))
 	}
 	analysisFilePath = regexp.MustCompile("경로:(.*)\\.log").FindString(analysisFilePath)
 	if len(analysisFilePath) == 0 {
-		return "", nil, errors.New("")
+		return "", nil, errors.New(fmt.Sprint("당첨번호 예측 결과가 저장되어 있는 파일의 경로를 찾을 수 없습니다. 자세한 내용은 로그를 확인하여 주세요."))
 	}
-	analysisFilePath = analysisFilePath[len("경로:"):]
+	analysisFilePath = string([]rune(analysisFilePath)[3:]) // '경로:' 문자열을 제거한다.
 
-	// 추출된 당첨번호 정보를 읽어들인다.
+	// 당첨번호 예측 결과 파일을 읽어들인다.
 	data, err := ioutil.ReadFile(analysisFilePath)
 	if err != nil {
 		return "", nil, err
 	}
 
+	// 당첨번호 예측 결과를 추출한다.
 	analysisResultData := string(data)
 	index := strings.Index(analysisResultData, "- 분석결과")
 	if index == -1 {
-		return "", nil, errors.New(fmt.Sprintf("%s", analysisFilePath)) //@@@@@
+		return "", nil, errors.New(fmt.Sprintf("당첨번호 예측 결과 파일의 내용이 유효하지 않습니다. 자세한 내용은 로그를 확인하여 주세요.\r\n(%s)", analysisFilePath))
 	}
 	analysisResultData = analysisResultData[index:]
 
 	message = regexp.MustCompile("당첨 확률이 높은 당첨번호 목록\\([0-9]+개\\)중에서 [0-9]+개의 당첨번호가 추출되었습니다.").FindString(analysisResultData)
 	message += "\r\n\r\n"
-	message += "- " + regexp.MustCompile("당첨번호1(.*)").FindString(analysisResultData) + "\r\n"
-	message += "- " + regexp.MustCompile("당첨번호2(.*)").FindString(analysisResultData) + "\r\n"
-	message += "- " + regexp.MustCompile("당첨번호3(.*)").FindString(analysisResultData) + "\r\n"
-	message += "- " + regexp.MustCompile("당첨번호4(.*)").FindString(analysisResultData) + "\r\n"
-	message += "- " + regexp.MustCompile("당첨번호5(.*)").FindString(analysisResultData)
-	//////////////////////////////////
+	message += "- " + utils.CleanString(regexp.MustCompile("당첨번호1(.*)").FindString(analysisResultData)) + "\r\n"
+	message += "- " + utils.CleanString(regexp.MustCompile("당첨번호2(.*)").FindString(analysisResultData)) + "\r\n"
+	message += "- " + utils.CleanString(regexp.MustCompile("당첨번호3(.*)").FindString(analysisResultData)) + "\r\n"
+	message += "- " + utils.CleanString(regexp.MustCompile("당첨번호4(.*)").FindString(analysisResultData)) + "\r\n"
+	message += "- " + utils.CleanString(regexp.MustCompile("당첨번호5(.*)").FindString(analysisResultData))
 
 	if t.IsCanceled() == true {
 		return "", nil, nil
