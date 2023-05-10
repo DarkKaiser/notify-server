d089f26ac2df9cf1e1bbccdc85728e1c734503ab
DarkKaiser <darkkaiser78@hotmail.com>
2023-05-10 15:50:54 +0900

    refactor: 전체 코드 리팩토링

--- service/api/handler/handler.go
+++ service/api/handler/handler.go
@@ -1,24 +1,21 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/darkkaiser/notify-server/g"
 	"github.com/darkkaiser/notify-server/service/api/model"
 	"github.com/darkkaiser/notify-server/service/notification"
-	"github.com/labstack/echo"
-	"net/http"
 )
 
 //
-// NotifyAPIHandlers
+// Handler
 //
-type NotifyAPIHandlers struct {
+type Handler struct {
 	allowedApplications []*model.AllowedApplication
 
 	notificationSender notification.NotificationSender
 }
 
-func NewNotifyAPIHandlers(config *g.AppConfig, notificationSender notification.NotificationSender) *NotifyAPIHandlers {
+func NewHandler(config *g.AppConfig, notificationSender notification.NotificationSender) *Handler {
 	// 허용된 Application 목록을 구한다.
 	var applications []*model.AllowedApplication
 	for _, application := range config.NotifyAPI.Applications {
@@ -30,28 +27,9 @@ func NewNotifyAPIHandlers(config *g.AppConfig, notificationSender notification.N
 		})
 	}
 
-	return &NotifyAPIHandlers{
+	return &Handler{
 		allowedApplications: applications,
 
 		notificationSender: notificationSender,
 	}
-}
-
-func (h *NotifyAPIHandlers) NotifyMessageSendHandler(c echo.Context) error {
-	m := new(model.NotifyMessage)
-	if err := c.Bind(m); err != nil {
-		return err
-	}
-
-	for _, application := range h.allowedApplications {
-		if application.ID == m.ApplicationID {
-			h.notificationSender.Notify(application.DefaultNotifierID, application.Title, m.Message, m.ErrorOccurred)
-
-			return c.JSON(http.StatusOK, map[string]int{
-				"result_code": 0,
-			})
-		}
-	}
-
-	return echo.NewHTTPError(http.StatusUnauthorized, fmt.Sprintf("접근이 허용되지 않은 Application입니다(ID:%s)", m.ApplicationID))
 }
--- service/api/router/router.go
+++ service/api/router/router.go
@@ -1,17 +1,14 @@
 package router
 
 import (
-	"github.com/darkkaiser/notify-server/g"
-	"github.com/darkkaiser/notify-server/service/api/handler"
 	_middleware_ "github.com/darkkaiser/notify-server/service/api/middleware"
-	"github.com/darkkaiser/notify-server/service/notification"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
-func New(config *g.AppConfig, notificationSender notification.NotificationSender) *echo.Echo {
+func New() *echo.Echo {
 	e := echo.New()
 
 	e.Debug = true
@@ -22,10 +19,12 @@ func New(config *g.AppConfig, notificationSender notification.NotificationSender
 	e.Logger = _middleware_.Logger{Logger: log.StandardLogger()}
 	e.Use(_middleware_.LogrusLogger())
 	// echo 기본 로그출력 구문, 필요치 않음!!!
-	//e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-	//	Format: `time="${time_rfc3339}" level=${level} remote_ip="${remote_ip}" host="${host}" method="${method}" uri="${uri}" user_agent="${user_agent}" ` +
-	//		`status=${status} error="${error}" latency=${latency} latency_human="${latency_human}" bytes_in=${bytes_in} bytes_out=${bytes_out}` + "\n",
-	//}))
+	/*
+		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: `time="${time_rfc3339}" level=${level} remote_ip="${remote_ip}" host="${host}" method="${method}" uri="${uri}" user_agent="${user_agent}" ` +
+				`status=${status} error="${error}" latency=${latency} latency_human="${latency_human}" bytes_in=${bytes_in} bytes_out=${bytes_out}` + "\n",
+		}))
+	*/
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{ // CORS Middleware
 		AllowOrigins: []string{"*"},
@@ -33,21 +32,6 @@ func New(config *g.AppConfig, notificationSender notification.NotificationSender
 	}))
 	e.Use(middleware.Recover()) // Recover from panics anywhere in the chain
 	e.Use(middleware.Secure())
-
-	grp := e.Group("/api/notify")
-	{
-		grp.Use(middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
-			KeyLookup:  "header:Authorization",
-			AuthScheme: "Bearer",
-			Validator: func(key string, c echo.Context) (bool, error) {
-				return key == config.NotifyAPI.APIKey, nil
-			},
-		}))
-
-		h := handler.NewNotifyAPIHandlers(config, notificationSender)
-
-		grp.POST("/message/send", h.NotifyMessageSendHandler)
-	}
 
 	return e
 }
