e2f69c85b7908c206bee48a78c30c4f4555ec160
DarkKaiser <darkkaiser@gmail.com>
2025-12-04 10:23:34 +0900

    refactor(strutils): FormatCommas 성능 최적화 및 코드 개선
    - FormatCommas: 정규식 제거하고 strings.Builder 사용으로 성능 향상
    - NormalizeMultiLineSpaces: Go 1.24 strings.SplitSeq 활용
    - 변수명 개선: ret→result, trimLine→normalizedLine, lines→lineIter
    - commaRegexp 정규식 변수 제거

--- pkg/strutils/strutils.go
+++ pkg/strutils/strutils.go
@@ -10,9 +10,6 @@ var (
 	// ToSnakeCase에서 사용하는 정규식
 	matchFirstRegexp = regexp.MustCompile("(.)([A-Z][a-z]+)")
 	matchAllRegexp   = regexp.MustCompile("([a-z0-9])([A-Z])")
-
-	// FormatCommas에서 사용하는 정규식
-	commaRegexp = regexp.MustCompile(`(\d+)(\d{3})`)
 )
 
 // ToSnakeCase CamelCase 문자열을 snake_case로 변환합니다.
@@ -32,48 +29,82 @@ func NormalizeSpaces(s string) string {
 // NormalizeMultiLineSpaces 여러 줄 문자열의 각 줄을 정규화하고 연속된 빈 줄을 하나로 축약합니다.
 // 앞뒤의 빈 줄도 제거됩니다.
 func NormalizeMultiLineSpaces(s string) string {
-	var ret []string
+	var result []string
 	var appendedEmptyLine bool
 
-	lines := strings.Split(s, "\n")
-	for _, line := range lines {
-		trimLine := NormalizeSpaces(line)
-		if trimLine != "" {
+	lineIter := strings.SplitSeq(s, "\n")
+	for line := range lineIter {
+		normalizedLine := NormalizeSpaces(line)
+		if normalizedLine != "" {
 			appendedEmptyLine = false
-			ret = append(ret, trimLine)
+			result = append(result, normalizedLine)
 		} else {
 			if !appendedEmptyLine {
 				appendedEmptyLine = true
-				ret = append(ret, "")
+				result = append(result, "")
 			}
 		}
 	}
 
 	// 앞뒤의 빈 줄 제거
-	if len(ret) >= 2 {
-		if ret[0] == "" {
-			ret = ret[1:]
+	if len(result) >= 2 {
+		if result[0] == "" {
+			result = result[1:]
 		}
-		if len(ret) > 0 && ret[len(ret)-1] == "" {
-			ret = ret[:len(ret)-1]
+		if len(result) > 0 && result[len(result)-1] == "" {
+			result = result[:len(result)-1]
 		}
 	}
 
-	return strings.Join(ret, "\r\n")
+	return strings.Join(result, "\r\n")
 }
 
 // FormatCommas 숫자를 천 단위 구분 기호(,)가 포함된 문자열로 변환합니다.
 // 예: 1234567 -> "1,234,567"
 func FormatCommas(num int) string {
-	str := fmt.Sprintf("%d", num)
-	for {
-		result := commaRegexp.ReplaceAllString(str, "$1,$2")
-		if result == str {
-			break
-		}
-		str = result
+	if num == 0 {
+		return "0"
 	}
-	return str
+
+	str := fmt.Sprintf("%d", num)
+
+	// 음수 처리
+	startOffset := 0
+	if num < 0 {
+		startOffset = 1
+	}
+
+	// 콤마가 필요 없는 경우 (3자리 이하)
+	if len(str)-startOffset <= 3 {
+		return str
+	}
+
+	var builder strings.Builder
+
+	// 예상 크기 미리 할당: 원래 길이 + 콤마 개수
+	commaCount := (len(str) - startOffset - 1) / 3
+	builder.Grow(len(str) + commaCount)
+
+	if num < 0 {
+		builder.WriteByte('-')
+		str = str[1:]
+	}
+
+	// 첫 번째 그룹 (1~3자리)
+	firstGroupLen := len(str) % 3
+	if firstGroupLen == 0 {
+		firstGroupLen = 3
+	}
+
+	builder.WriteString(str[:firstGroupLen])
+
+	// 나머지 그룹들 (3자리씩)
+	for i := firstGroupLen; i < len(str); i += 3 {
+		builder.WriteByte(',')
+		builder.WriteString(str[i : i+3])
+	}
+
+	return builder.String()
 }
 
 // SplitAndTrim 주어진 구분자로 문자열을 분리한 후, 각 항목의 앞뒤 공백을 제거하고 빈 문자열을 제외한 슬라이스를 반환합니다.
